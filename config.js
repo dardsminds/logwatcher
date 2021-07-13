c2d10c82c1676d19ebe2b1fb4de1114aeaa1d599
Dario Mindoro <dario.mindoro@dealereprocess.com>
2021-07-13 15:00:26 +0000

    adding and implementing configuration from external json file

---
+++ config.js
@@ -0,0 +1,21 @@
+let config_data = null
+module.exports = function() {
+	if(config_data != null && config_data != undefined) {
+		return config_data
+	}
+		
+	config_data = {}
+
+	if(process.env.NODE_ENV === undefined || process.env.NODE_ENV == null || process.env.NODE_ENV == 'development') { 
+		config_data = require('./config/config.dev.json')
+	} else {
+	if(process.env.NODE_ENV == 'production') {
+		config_data = require('./config/config.prod.json')
+	}}
+
+	config_data.connection_string = process.env.connection_string 
+	config_data.port = process.env.port || config_data.port
+	
+	return config_data
+}
+
