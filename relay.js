fef68fd8c28a4c5882ce3aa91f7cf70369424f37
Dario Mindoro <dario.mindoro@dealereprocess.com>
2021-07-13 17:32:49 +0000

    adding relay server

---
+++ relay.js
@@ -0,0 +1,27 @@
+var config = require("./config")();
+
+const WebSocket = require('ws');
+const ws = new WebSocket(config.echoserver);
+
+ws.on('open', function open() {
+	ws.send('Im relay connected');
+	console.log("Connection established to echo server.");
+});
+
+var logfile = config.logfile;
+
+Tail = require('tail').Tail;
+tail = new Tail(logfile);
+
+var dataBuffer;
+
+tail.on("line", function(data) {
+
+	dataBuffer =  data;
+	ws.send(dataBuffer);
+});
+
+tail.on("error", function(error) {
+
+	console.log('ERROR: ', error);
+});
