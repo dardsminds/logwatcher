5d24664e0a1bf117ce99b43184784629572c89f1
Dario Mindoro <dario.mindoro@dealereprocess.com>
2022-02-16 04:31:26 +0000

    add controls and placeholders

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+	<>
+		<div class="toolbar">
+		<div class="controls">
+		<button onClick="reload_data()">Reload</button>
+		<button onClick="clear_data()">Clear</button>
+		<input type="checkbox" id="clear_for_new_data" /> Auto-clear
+		</div>
+		<div id="ws_server_status" class="status_display"></div>
+		</div>
+		<div id="server_messages" class="language-java code"></div>
+	</>,
   document.getElementById('root')
 );
