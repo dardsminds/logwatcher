4b2546482b70ea0ac4f50deb13ffa2a8ba411367
Dario Mindoro <dario.mindoro@dealereprocess.com>
2022-02-16 04:21:22 +0000

    add css loader

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -9,16 +9,20 @@ module.exports = {
   mode: "development",
   module: {
     rules: [
-      {
-        test: /\.?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
+		{
+		test: /\.?js$/,
+		exclude: /node_modules/,
+		use: {
+				loader: "babel-loader",
+				options: {
+				presets: ['@babel/preset-env', '@babel/preset-react']
+				}
+			}
+		},
+		{
+			test: /\.css$/,
+			use: [ 'style-loader', 'css-loader' ]
+		}
     ]
   },
   plugins: [
